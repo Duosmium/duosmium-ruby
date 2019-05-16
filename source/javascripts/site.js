1ee6fc72e3d99b95eaf030923669f8dd84cbfdb2
Em Zhan <zqianem@gmail.com>
2019-05-16 18:41:14 -0400

    Highlight event columns on hover
    Still need a good way to highlight the rotated th tags, but this will do
    for now.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,13 @@
 // This is where it all goes :)
+
+// Adapted from https://css-tricks.com/row-and-column-highlighting/
+$(function(){
+  $("table.results-classic").delegate('td','mouseover mouseleave', function(e) {
+    if (e.type == 'mouseover') {
+      $("colgroup col").eq($(this).index()).addClass("hover");
+    }
+    else {
+      $("colgroup col").eq($(this).index()).removeClass("hover");
+    }
+  });
+})
