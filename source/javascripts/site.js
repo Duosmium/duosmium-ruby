aa705067828ae39d82d687b5b285e046fe7c0c41
Em Zhan <zqianem@gmail.com>
2019-05-30 23:23:05 -0400

    Make logo clickable to toggle tournament summary

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -188,5 +188,15 @@ $(document).ready(function(){
   });
   $("div.card-body div.summary").on("hide.bs.collapse", function() {
     $(this).parent().children("img").removeClass("blur");
+    // Unfocus tournament summary button when summary is hidden
+    let button = $(this).parent().parent().find("div.card-actions button")
+    button.blur(); // removes the focus, nothing to do with visual blur
+  });
+
+  // Make tournament summary toggle when clicking image/summary
+  $("div.card-body img, div.card-body div.summary").on("click", function() {
+    let button = $(this).parent().parent().find("div.card-actions button")
+    button.focus();
+    button.click();
   });
 });
