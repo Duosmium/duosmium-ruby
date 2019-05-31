efd1a2cf7c174cee1982a0914439617f380bd03f
Em Zhan <zqianem@gmail.com>
2019-05-31 00:46:30 -0400

    Make card body the data toggler instead of button
    Fixes some awkward auto scrolling that tried to put the Summary button
    on screen because it got focus, but would scroll past the tournament
    title.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -189,14 +189,12 @@ $(document).ready(function(){
   $("div.card-body div.summary").on("hide.bs.collapse", function() {
     $(this).parent().children("img").removeClass("blur");
     // Unfocus tournament summary button when summary is hidden
-    let button = $(this).parent().parent().find("div.card-actions button")
+    let button = $(this).parent().parent().find("div.card-actions button");
     button.blur(); // removes the focus, nothing to do with visual blur
   });
 
-  // Make tournament summary toggle when clicking image/summary
-  $("div.card-body").on("click", function() {
-    let button = $(this).parent().find("div.card-actions button")
-    button.focus();
-    button.click();
+  // Make tournament summary toggle when clicking summary button
+  $("div.card-actions button").on("click", function() {
+    $(this).parent().parent().children("div.card-body").click();
   });
 });
