5274bc056acb72989b50b8a79f934c3b3a67a791
Em Zhan <zqianem@gmail.com>
2019-06-05 18:22:51 -0400

    Minimize CSS
    Could optimize this more later by not importing parts of Material that
    aren't being used.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
   mode: "production",
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
   plugins: [
     new MiniCssExtractPlugin()
   ],
