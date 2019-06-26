2696dc18d96defb6b9e9581c8660d368d2ca8314
Em Zhan <zqianem@gmail.com>
2019-06-26 14:00:07 -0400

    Fix lack of focus indication on checkboxes
    Noticeable when navigating with keyboard

--- assets/results_template/index.js
+++ assets/results_template/index.js
@@ -3,3 +3,4 @@ import './style.scss';
 import './script.js';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/popover';
+import 'daemonite-material/assets/js/src/selection-control-focus'
