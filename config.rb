cf079f691cff2c2e1953efd9464e8093108a0035
Em Zhan <zqianem@gmail.com>
2019-06-28 09:05:00 -0400

    Add support for SVG tournament logos
    Hopefully this works on various browsers? 👀 Chrome

--- config.rb
+++ config.rb
@@ -103,9 +103,11 @@ helpers do
     potential_logos = [
       filename + '.png',
       filename + '.jpg',
+      filename.split('_')[0..-2].join('_') + '.svg', # remove _b or _c suffix
       filename.split('_')[0..-2].join('_') + '.png', # remove _b or _c suffix
       filename.split('_')[0..-2].join('_') + '.jpg', # remove _b or _c suffix
       filename.split('_')[0..-2].join('_') + '.gif', # remove _b or _c suffix
+      filename.split('_')[1..-2].join('_') + '.svg', # remove date as well
       filename.split('_')[1..-2].join('_') + '.png', # remove date as well
       filename.split('_')[1..-2].join('_') + '.jpg', # remove date as well
       filename.split('_')[1..-2].join('_') + '.gif'  # remove date as well
