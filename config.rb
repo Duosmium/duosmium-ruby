fc1d84ae64171b2eca9ffa651e72e062d2b66cb5
Em Zhan <zqianem@gmail.com>
2019-10-17 03:36:27 -0400

    Clean up unused default comments/code in config.rb

--- config.rb
+++ config.rb
@@ -1,18 +1,4 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 require 'miro'
-
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
@@ -26,14 +12,6 @@ Dir.new(Pathname.new(__dir__) + 'data')
   proxy "/results/#{t}.html", "/results/template.html",
     locals: { tournament: t }, ignore: true
 end
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
 
 # Helpers
 # Methods defined in the helpers block are available in templates
@@ -251,16 +229,11 @@ helpers do
   end
 end
 
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
 activate :external_pipeline,
   name: :webpack,
   command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
   source: ".tmp/dist",
   latency: 1
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
