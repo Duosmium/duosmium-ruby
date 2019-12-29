b27125a7668d2c08a41fc091a1419554e505d7d7
Em Zhan <zqianem@gmail.com>
2019-12-29 00:05:56 -0500

    Prettify other results link when nonlocal
    In coordination with Netlify's URL prettifying

--- assets/results_template/script.js
+++ assets/results_template/script.js
@@ -272,6 +272,9 @@ $(document).ready(function(){
     $("div#team-detail span#team").html(source_row.attr("data-team-name"));
     $("div#team-detail span#school").html(source_row.attr("data-school"));
     let h = "schools.html#" + source_row.attr("data-school").replace(/ /g, "_");
+    if (window.location.href.startsWith("https://unosmium.org")) {
+      h = h.replace(".html", "");
+    }
     $("a#other-results").attr("href", h);
 
     let table_rows = $("div#team-detail table tbody").children();
