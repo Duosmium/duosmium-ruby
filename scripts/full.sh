c03e9e1f7959986d5c996eece39392ad6926f93f
Shreyas Mayya <smayya337@gmail.com>
2021-04-30 12:32:40 -0400

    update script

--- scripts/full.sh
+++ scripts/full.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if [[ $NETLIFY -gt 0 ]]
+if [ $NETLIFY -gt 0 ]
 then
+  echo "In Netlify, installing Python stuff..."
   apt -y install python3-pip
   pip install PyYAML GitPython
 fi
-python3 $(dirname $0)"/recents-update.py"
+python3 "$(dirname $0)/recents-update.py"
 bundle exec middleman build --verbose "$@"
 cp redirects/*.html build/
