74a3e6e3aa837abdf3240f2af30d231214f8ba8a
Shreyas Mayya <smayya337@gmail.com>
2021-05-10 09:37:52 -0400

    some new stuff [no ci]

--- scripts/full.sh
+++ scripts/full.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ $NETLIFY = "1" ]
-then
-  echo "In Netlify, installing Python stuff..."
-  apt -y install python3-pip
-  pip install PyYAML GitPython
-fi
+#if [ $NETLIFY = "1" ]
+#then
+#  echo "In Netlify, installing Python stuff..."
+#  apt -y install python3-pip
+#  pip install PyYAML GitPython
+#fi
 python3 "$(dirname $0)/recents-update.py"
 bundle exec middleman build --verbose "$@"
 cp redirects/*.html build/
